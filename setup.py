9cbc9c2093d98a8f4d50364f1366f0f5812fd145
Krunal Dalvi <dalvikrunal15@gmail.com>
2024-10-22 23:51:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,16 @@ setup(
     install_requires=[
         "numpy", 
     ],
-    author="Krunal Chandrakant Dalvi",
-    description="A Python package for XTS (XOR-Transposition-Substitution) cipher encryption and decryption",
+    description='A Python package for encryption and decryption using the XOR-Transposition-Substitution (XTS) cipher algorithm.',
+    long_description=open('README.md').read(),  # Read from your README file
+    long_description_content_type='text/markdown',  # Specify that the long description is in Markdown
+    author='Krunal Chandrakant Dalvi',
+    author_email='dalvikrunal15@gmail.com',
+    url='https://github.com/KrunalDalvi/xts_cipher',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
 )
